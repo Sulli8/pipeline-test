f2eee6f47db9f4e33e35bef6fa709348f69216cf
Sulli8 <sullivan.sextius@gmail.com>
2026-08-24 14:42:31 +0200

    + add another scenarios

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -12,6 +12,13 @@ test('scenarios 002', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
+test('scenarios 003', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
